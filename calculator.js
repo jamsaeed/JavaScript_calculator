8f85200256dfa3fb367d328a991e314c863c40d0
jamsaeed <jameela.mohamed.saeed@gmail.com>
2025-12-14 19:52:29 +0300

    basic setup

---
+++ calculator.js
@@ -0,0 +1,23 @@
+// select all numbers
+const numbers = document.querySelectorAll(".nums")
+
+//  select Arithmetic
+const add = document.querySelector(".add")
+const sub = document.querySelector(".sub")
+const multiply = document.querySelector(".mul")
+const divide = document.querySelector(".div")
+
+// select decimal
+const decimal = document.querySelector(".decimal")
+
+// select equal
+const equal = document.querySelector(".equal")
+
+//select delete
+// back space
+const backSpace = document.querySelector("#back-space")
+// All clear AC
+const clearAll = document.querySelector('#clear')
+
+// select display input
+const display = document.querySelector(".display")
